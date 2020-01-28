b7de130caa37765cd9bc75ce07e005997ff35e35
nguyena7 <nguyena7@users.noreply.github.com>
2020-01-28 10:53:23 -0500

    Done I think

---
+++ playfair.m
@@ -0,0 +1,191 @@
+clear all; close all; clc;
+
+%keyword = input("Enter the KEYWORD: ", 's');
+keyword = "CINCINNATI";
+message = "A TEST TO ENCRYPT USING PLAYFAIR CIPHER";
+
+% Prepare message for encryption
+nospace = no_space(message);
+nodups = check_dups(nospace);
+split = get_pairs(nodups)';
+nodup = remove_dup(keyword);
+
+% Create matrix
+mat = make_matrix(nodup);
+
+% Encrypt message
+encrypted_message = encrypt(mat, split);
+
+% Prepare for message decryption
+enc_splitted = get_pairs(encrypted_message)';
+
+% Decrypt message
+decrypted_message = decrypt(mat, enc_splitted);
+decrypted_message = erase(decrypted_message, "X"); % Remove X in message
+
+% Display output
+disp("Playfair Matrix:");
+disp(mat);
+
+disp("Original Message:");
+disp(message);
+
+disp("Encrypted Message:");
+disp(encrypted_message);
+
+disp("Decrypted Message:");
+disp(decrypted_message);
+
+%% Break string into pairs
+function split_txt = get_pairs(plain_txt)
+    split_txt = "";
+    for i = 1:strlength(plain_txt)
+        if i == 1
+            split_txt = append(split_txt, plain_txt{1}(i));
+        elseif mod(i-1, 2) ~= 0
+            split_txt = append(split_txt, plain_txt{1}(i));
+        else
+            split_txt = append(split_txt, ' ');
+            split_txt = append(split_txt, plain_txt{1}(i));
+        end
+    end 
+    split_txt = split(split_txt);
+end
+
+%% Removing spaces from a string
+function str = no_space(message)
+    str = regexprep(message, '\s+', '');
+end
+
+%% Add X's for duplicates or odd numbered strings
+function padded = check_dups(message)
+    padded = "";
+    for i = 1:strlength(message)
+        if i == strlength(message)
+            curr_char = message{1}(i);
+            padded = append(padded, curr_char);   
+            if mod(strlength(padded), 2 )~= 0
+                padded = append(padded, 'X');
+            end
+        elseif i ~= strlength(message)
+            curr_char = message{1}(i);
+            next_char = message{1}(i+1);
+
+            if(curr_char == next_char)
+                padded = append(padded, curr_char);
+                padded = append(padded, 'X');
+            else
+                padded = append(padded, curr_char);
+            end
+        end
+    end
+end
+
+%% Remove duplicate characters from keyword
+function uniq_str = remove_dup(str)
+    uniq_str = "";
+    for i = 1:strlength(str)
+        if contains(uniq_str, str{1}(i)) == 0
+            uniq_str = append(uniq_str, str{1}(i));
+        end
+    end
+end
+
+%% Making the 5x5 matrix
+function matrix = make_matrix(kw)
+    mat_str = kw; 
+    alph = 'A':'Z';
+    alph(find(alph == 'J')) = []; % Remove J from alphabet
+
+    % combining the keyword and the alphabet to insert into matrix
+    for i = 1:strlength(alph)
+        if contains(kw, alph(i)) == 0
+            mat_str = append(mat_str, alph(i));
+        end
+    end
+    
+    matrix = strings(5);
+    index = 1;
+    for row = 1:5   
+        for col = 1:5
+            %matrix(row, col) = str2arr(index);
+            matrix(row, col) = mat_str{1}(index);
+            index = index + 1;
+        end
+    end
+end
+
+%% Encrypting the message
+function enc_message = encrypt(matrix, plaintext)
+    enc_message = "";
+    for i = 1:length(plaintext)
+        char1 = plaintext{i}(1);
+        char2 = plaintext{i}(2);
+        
+        if char1 == "J"
+            char1 = "I";
+        elseif char2 == "J"
+            char2 = "I";
+        end
+        
+        [row1, col1] = find(matrix == char1);
+        [row2, col2] = find(matrix == char2);
+        
+         if row1 == row2
+            enc_message = append(enc_message, matrix(row1, mod(col1, 5) + 1));
+            enc_message = append(enc_message, matrix(row2, mod(col2, 5) + 1));
+         elseif col1 == col2
+            enc_message = append(enc_message, matrix(mod(row1, 5) + 1, col1));
+            enc_message = append(enc_message, matrix(mod(row2, 5) + 1, col2));
+         else
+            enc_message = append(enc_message, matrix(row1, col2));
+            enc_message = append(enc_message, matrix(row2, col1));
+         end
+    end
+end
+
+%% Decrypting the message
+function denc_message = decrypt(matrix, message)
+    denc_message = "";
+    for i = 1:length(message)
+        char1 = message{i}(1);
+        char2 = message{i}(2);
+        
+        if char1 == "J"
+            char1 = "I";
+        elseif char2 == "J"
+            char2 = "I";
+        end
+        
+        [row1, col1] = find(matrix == char1);
+        [row2, col2] = find(matrix == char2);
+        
+         if row1 == row2
+            a = mod(col1 - 1, 5)
+            if a == 0
+                a = 5;
+            end
+            b = mod(col2 - 1, 5)
+            if b == 0
+                b = 5;
+            end
+            
+            denc_message = append(denc_message, matrix(row1, a));
+            denc_message = append(denc_message, matrix(row2, b));
+         elseif col1 == col2
+            a = mod(row1 - 1, 5)
+            if a == 0
+                a = 5;
+            end
+            b = mod(row2 - 1, 5)
+            if b == 0
+                b = 5;
+            end
+            denc_message = append(denc_message, matrix(a, col1));
+            denc_message = append(denc_message, matrix(b, col2));
+         else
+            denc_message = append(denc_message, matrix(row1, col2));
+            denc_message = append(denc_message, matrix(row2, col1));
+         end
+    end
+end
---
+++ stenography.m
@@ -0,0 +1,95 @@
+clear all; close all; clc;
+
+lena = imread('lena.png');
+height = size(lena, 1);
+width = size(lena, 2);
+figure, imshow(lena);
+
+msg = '101451472';
+
+% Split msg digits 
+split = num2cell(msg);
+for i = 1 : size(split,2)
+    temp = split{i}(1);
+    new = str2num(temp);
+    ID_arr(i) = new;
+end
+
+%% Embedding message in picture
+disp("Message:");
+disp(msg);
+disp("Embedding the message into the picture");
+ID_len = length(ID_arr);
+output = lena;
+pixel_count = 1;
+for h = 1 : height
+    for w = 1 : width
+        if pixel_count <= ID_len
+            % Get pixel's decimal value...
+            dec = lena(h,w);
+            % ... Convert to binary
+            bin_pix = de2bi(dec,9);
+
+            % Get ID digit
+            temp = ID_arr(pixel_count);
+            % Convert digit to binary
+            bin_temp = de2bi(temp,4);
+
+            % Embed into pixel
+            bin_pix(1:4) = bin_temp;
+            embedded = bin_pix;
+            
+            % Convert the binary to decimal
+            embedded = bi2de(embedded);
+
+            % Embed the new value into the output
+            output(h, w) = embedded;
+
+            % Increment pixel_cout
+            pixel_count = pixel_count + 1;
+        end
+    end
+end
+figure, imshow(output);
+
+%% Extracting message from output
+disp("Extracting message");
+decrypted_ID = zeros(1,9);
+pixel_count = 1;
+for h2 = 1 : height
+    for w2 = 1 : width
+        if pixel_count <= ID_len
+            % Create new array to store extracted bits
+            first_four = zeros(1,4);
+            
+            % Extracting pixel ...
+            pix_dec = output(h2, w2);
+            % ... Convert to binary
+            bin_pix = de2bi(pix_dec,9);
+            
+            % Extract first 4 bits from pixel
+            for pix_idx = 1:4 
+                first_four(pix_idx) = bin_pix(pix_idx);
+            end
+
+            % Convert binary to string 
+            first_four = num2str(first_four);
+            first_four(isspace(first_four)) = '';
+            
+            % Flip the bits
+            first_four = flip(first_four);
+            
+            % Convert the string binary to decimal
+            decimal = bin2dec(first_four);
+            
+            % Store in array
+            decrypted_ID(pixel_count) = decimal;
+            
+            % Increment pixel_cout
+            pixel_count = pixel_count + 1;
+        end
+    end
+end
+
+disp("Extracted Message:");
+disp(decrypted_ID);
